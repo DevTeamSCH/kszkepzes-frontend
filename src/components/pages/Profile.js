610b77f0da57d6bcd44c713c933c4fd4ad233d3f
rlacko <rlacko@sch.bme.hu>
2020-02-10 12:12:45 +0100

    fixed link

--- src/components/pages/Profile.js
+++ src/components/pages/Profile.js
@@ -122,11 +122,11 @@ class Profile extends Component {
                   </li>
                   <br />
                   <li>
-                  Találsz egy értelmetlen szöveget egy honlapon (például: <a href='http://kszkepzes18.sch.bme.hu/zebra.html'>http://kszkepzes18.sch.bme.hu/zebra.html</a>), de feltűnik, hogy két egyenlőségjellel fejeződik be. Nyomozz, s a végeredményt (amit találtál) írd ide!
+                  Találsz egy értelmetlen szöveget egy honlapon (például: <a href='https://ujonc.kszk.bme.hu/zebra.html'>https://ujonc.kszk.bme.hu/zebra.html</a>), de feltűnik, hogy két egyenlőségjellel fejeződik be. Nyomozz, s a végeredményt (amit találtál) írd ide!
                   </li>
                   <br />
                   <li>
-                  A <b>kszkepzes18.sch.bme.hu</b> címen elérhető gépen
+                  A <b>ujonc.kszk.bme.hu</b> címen elérhető gépen
                   fut egy szolgáltatás az alapértelmezett <b>5432</b> porton
                   (használd az előző feladatban kapott adatokat).
                   Belépés után keresd meg a feladat megoldását!
